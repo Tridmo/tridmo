314dadde6c1dedb61010487dd56c8a48412a7295
farruholimov <farruhbekolimov3904@gmail.com>
2024-08-11 17:02:32 +0500

    update

--- src/components/views/footer/index.tsx
+++ src/components/views/footer/index.tsx
@@ -51,31 +51,26 @@ export default function Footer() {
   const socialMedia = [
     {
       icon: "/icons/instagram-icon.svg",
-      url: "/",
+      url: "https://www.instagram.com/demod.uz/",
       margin: true
     },
     {
       icon: "/icons/telegram-icon.svg",
-      url: "/",
+      url: "https://t.me/demoduz",
       margin: true
     },
     {
-      icon: "/icons/facebook-icon.svg",
-      url: "/",
+      icon: "/icons/chat.svg",
+      url: "https://t.me/demod_uz",
       margin: true
     },
-    {
-      icon: "/icons/twitter-icon.svg",
-      url: "/",
-      margin: false
-    }
   ]
 
   return (
     <footer style={{ marginTop: "auto" }}>
       <Box sx={{ width: "100%", flexGrow: 1, borderBottom: "1px solid #e0e0e0", paddingTop: "48px", background: "#fff" }}>
         <Grid container spacing={2} sx={{ maxWidth: "1200px", margin: "0 auto" }}>
-          <Grid item xs={6} sx={{ padding: "0 !important", display: "flex", justifyContent: "flex-start" }}>
+          <Grid item xs={4} sx={{ padding: "0 !important", display: "flex", justifyContent: "flex-start" }}>
             <Item sx={{ padding: "0", display: "flex", flexDirection: "column", alignItems: "start" }}>
               <Link href="/">
                 <Image
@@ -87,7 +82,51 @@ export default function Footer() {
               </Link>
             </Item>
           </Grid>
-          {/* <Grid item xs={3} sx={{ padding: "0 !important", display: "flex", justifyContent: "center" }} >
+
+          <Grid item xs={4} sx={{ padding: "0 !important", display: "flex", justifyContent: "flex-start" }}></Grid>
+
+          <Grid item xs={4} sx={{ padding: "0 !important", display: "flex", justifyContent: "flex-end" }} >
+            <Item sx={{ width: '100%', padding: "0", display: 'flex', justifyContent: 'space-between' }}>
+              <Box>
+                <SimpleTypography text="Для сотрудничества" className='footer__title'></SimpleTypography>
+                <SimpleTypography text="+998 90 666 66 60" className='footer__title'></SimpleTypography>
+              </Box>
+
+              <Box>
+                <SimpleTypography text="Свяжитесь с нами" className='footer__title'></SimpleTypography>
+                <List sx={{ width: '100%', display: "flex", bgcolor: 'background.paper', padding: "0" }}>
+                  {
+                    socialMedia.map((item, i) => (
+                      <ListItem
+                        key={i}
+                        sx={{ width: 'auto', padding: "0", marginRight: `${item.margin ? "24px" : "0"}` }}
+                      >
+                        <Link href={item.url} target='_blank'>
+
+                          <Image
+                            width={20}
+                            height={20}
+                            alt="icon"
+                            src={item.icon}
+                          />
+
+                        </Link>
+                      </ListItem>
+                    ))
+                  }
+                </List>
+              </Box>
+            </Item>
+          </Grid>
+          <SimpleTypography text="2024 © Все права защищены." className='footer__desc'></SimpleTypography>
+        </Grid>
+      </Box>
+    </footer>
+  )
+}
+
+
+{/* <Grid item xs={3} sx={{ padding: "0 !important", display: "flex", justifyContent: "center" }} >
                         <Item sx={{ padding: "0" }}>
                             <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper', padding: "0" }}>
                                 {
@@ -127,35 +166,3 @@ export default function Footer() {
                             </List>
                         </Item>
                     </Grid> */}
-          <Grid item xs={6} sx={{ padding: "0 !important", display: "flex", justifyContent: "flex-end" }} >
-            <Item sx={{ padding: "0" }}>
-              <SimpleTypography text="Свяжитесь с нами" className='footer__title'></SimpleTypography>
-              <List sx={{ width: '100%', display: "flex", bgcolor: 'background.paper', padding: "0" }}>
-                {
-                  socialMedia.map((item, i) => (
-                    <ListItem
-                      key={i}
-                      sx={{ padding: "0", marginRight: `${item.margin ? "36px" : "0"}` }}
-                    >
-                      <Link href={item.url} target='_blank'>
-
-                        <Image
-                          width={20}
-                          height={20}
-                          alt="icon"
-                          src={item.icon}
-                        />
-
-                      </Link>
-                    </ListItem>
-                  ))
-                }
-              </List>
-            </Item>
-          </Grid>
-          <SimpleTypography text="© 2022 Demod Inc. Ташкент, Узбекистан." className='footer__desc'></SimpleTypography>
-        </Grid>
-      </Box>
-    </footer>
-  )
-}
